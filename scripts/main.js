fc6d2f32fc67cd96edc418017e19bb7ec38ddd37
trasnochado <alonginico@gmail.com>
2021-04-06 15:44:34 -0300

    new links, only gifs, new functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,80 +1,33 @@
 //selectors
-const t = document.querySelector(".t");
-const r = document.querySelector(".r");
-const a = document.querySelector(".a");
-const s = document.querySelector(".s");
-const n = document.querySelector(".n");
-const o = document.querySelector(".o");
-const c = document.querySelector(".c");
-const ab = document.querySelector(".ab");
-const h = document.querySelector(".h");
-const d = document.querySelector(".d");
-const ob = document.querySelector(".ob");
 const imagen = document.querySelector(".imagen");
+const sonrisa = document.querySelector(".sonrisa");
 
 //event listeners
-t.addEventListener('click', prueba);
-r.addEventListener('click', prueba);
-a.addEventListener('click', prueba);
-s.addEventListener('click', prueba);
-n.addEventListener('click', prueba);
-o.addEventListener('click', prueba);
-c.addEventListener('click', prueba);
-h.addEventListener('click', prueba);
-ab.addEventListener('click', prueba);
-d.addEventListener('click', prueba);
-ob.addEventListener('click', prueba);
-imagen.addEventListener('click', prueba);
+
+sonrisa.addEventListener('click', prueba);
 
 //functions
 
 let imagenes = [
-    'images/24m1.jpg',
-    'images/acab.jpg',
-    'images/army.jpg',
-    'images/artform.jpg',
-    'images/be kind.jpg',
-    'images/coffee2.jpg',
-    'images/contra.jpg',
-    'images/crown.jpg',
-    'images/cuarentena.jpg',
-    'images/decolonize.jpg',
-    'images/ecstasy.jpg',
-    'images/education.jpg',
-    'images/engels, marx and a red flag.jpg',
-    'images/expired.jpg',
-    'images/god prays.jpg',
-    'images/growth.jpg',
-    'images/interesting.jpg',
-    'images/invisible.jpg',
-    'images/lost.jpg',
-    'images/my life.jpg',
-    'images/my life2.jpg',
-    'images/numb.jpg',
-    'images/overpopulation.jpg',
-    'images/reinventadoprimeraedicion.jpg',
-    'images/routine verde.jpg',
     'images/superimposed interpretations of a 2012 laptop.gif',
     'images/superimposed interpretations of a calculator.gif',
     'images/superimposed interpretations of a pair of pliers.gif',
     'images/superimposed interpretations of a pendrive.gif',
     'images/superimposed interpretations of a tennis racquet.gif',
+    'images/superimposed interpretations of a hammer.gif',
+    'images/superimposed interpretations of a cap.gif',
 ];
-
-let inicial = Math.floor(Math.random() * ((imagenes.length - 1) - 25 + 1)) + 25;
-imagen.setAttribute('src', imagenes[inicial]);
 
 let max = imagenes.length - 1;
 let min = 0;
 
-function randomIndex(max, min) {
+let inicial = Math.floor(Math.random() * (max - min + 1)) + min;
+imagen.setAttribute('src', imagenes[inicial]);
 
+function randomIndex(max, min) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 function prueba() {
-
     imagen.setAttribute('src', imagenes[randomIndex(max, min)]);
-    //console.log(randomIndex(max, min));
-
 };
